48bce57784019978be9426b4c2213b597c40cf5a
James Truslow <j.g.truslow@gmail.com>
2018-06-09 13:38:40 -0400

    tested with cancer data, refactored test_rig(), now uses ROC AUC

--- ensemble.py
+++ ensemble.py
@@ -7,7 +7,6 @@ Created on Fri Jun  8 10:23:24 2018
 
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import cross_val_score
 
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
@@ -30,49 +29,106 @@ def ensemble_p(X, model_list, weight_list):
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 def ensemble_two_models(X, y, model_list):
-
+    """Calls GridSearchCV on a VotingCLassifier object.  Grid search 
+    is  performed over various weights given to the two estimators in 
+    the VotingClassifer.
+    
+    Paramters:
+    ----------
+    X:  array of predictor values, formatted for sklearn
+    y:  target values, formatted for sklearn
+        model_list: a list of two sklearn classifiers
+    
+    Returns:
+    --------
+    gscv:   A fitted GridSearchCV object.  Score for each cv 
+        is the ROC AUC (only works for binary classes)        
+    
+    """
+    
     from sklearn.ensemble import VotingClassifier
     from sklearn.model_selection import GridSearchCV
+
+    # Probably need to import here all classifier types that 
+    # might be sent to this function
     from sklearn.ensemble import RandomForestClassifier
+    from sklearn.linear_model import LogisticRegression
     
     vc = VotingClassifier([('m0', model_list[0]),('m1', model_list[1])],
                            voting='soft')
     
+    # put together the parameter list for different weights
+    # for the VotingClassifier
     w1 = np.linspace(0,1,11)
     w2 = 1 - w1
     w_array = [[w_one, w_two] for w_one, w_two in zip(w1,w2)]
-    # w1 and w2 are independantly varied.  This will allow a third weight, w3, to 
-    # be calculated via 1 = w1 + w2 + w3
+
+    
     gs_params = {'weights': w_array}
     
+    
     gscv = GridSearchCV(vc, gs_params,
-                        cv=5, return_train_score=True)
+                        cv=5, return_train_score=True, scoring = 'roc_auc')
     gscv.fit(X,y)
     
     return gscv
 
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-"""
-Test rig for GridSerach on ensemble classifier, built w/ iris data
-"""
+def CV_test_rig(X,y):
+    """
+    Tests GridSearchV with VotingClassifier composed of two 
+    RandomForestClassifiers
+    
+    Parameters:
+    ----------
+    X: predictors, sklearn format
+    y: targets, sklarn format
+    
+    Returns:
+    --------
+    gscv: a fitted GridSearchCv object
+    """
+    
+    from sklearn.ensemble import RandomForestClassifier
+    from sklearn.model_selection import GridSearchCV
+    
+    rfc1 = RandomForestClassifier(n_estimators = 5, max_depth = 3)
+    rfc2 = RandomForestClassifier(n_estimators = 3, max_depth = 5)
+    
+    from sklearn.model_selection import GridSearchCV
+    
+    gscv = ensemble_two_models(X,y, [rfc1, rfc2])
 
+    return gscv
+
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+"""
+Test rig for GridSearch on ensemble classifier, built w/ iris data
+"""
 
 from sklearn.datasets import load_iris
 
 data = load_iris()
+iris_y = data.target
+iris_X = data.data
+
+gscv = CV_test_rig(X,y)
+
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+"""
+Test rig for GridSearch on ensemble classifier, built w/ breast-cancer
+data
+"""
+
+from sklearn.datasets import load_breast_cancer
+data = load_breast_cancer()
 y = data.target
 X = data.data
 
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import GridSearchCV
-
-rfc1 = RandomForestClassifier(n_estimators = 5, max_depth = 3)
-rfc2 = RandomForestClassifier(n_estimators = 3, max_depth = 5)
-
-from sklearn.model_selection import GridSearchCV
-
-gscv = ensemble_two_models(X,y, [rfc1, rfc2])
+gscv = CV_test_rig(X,y)
 
 gscv.cv_results_
 gscv.best_score_
