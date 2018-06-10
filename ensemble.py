b3eee1d5cf964873783354e6e320e045f934e8a0
James Truslow <j.g.truslow@gmail.com>
2018-06-09 21:40:17 -0400

    added code for 3model ensemble, seems to be workingC

--- ensemble.py
+++ ensemble.py
@@ -33,7 +33,7 @@ def ensemble_two_models(X, y, model_list):
     is  performed over various weights given to the two estimators in 
     the VotingClassifer.
     
-    Paramters:
+    Parameters:
     ----------
     X:  array of predictor values, formatted for sklearn
     y:  target values, formatted for sklearn
@@ -65,8 +65,59 @@ def ensemble_two_models(X, y, model_list):
 
     
     gs_params = {'weights': w_array}
+
+    gscv = GridSearchCV(vc, gs_params,
+                        cv=5, return_train_score=True, scoring = 'roc_auc')
+    gscv.fit(X,y)
     
+    return gscv
+
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     
+
+def ensemble_three_models(X, y, model_list):
+    """Calls GridSearchCV on a VotingCLassifier object.  Grid search 
+    is  performed over various weights given to the two estimators in 
+    the VotingClassifer.
+    
+    Parameters:
+    ----------
+    X:  array of predictor values, formatted for sklearn
+    y:  target values, formatted for sklearn
+        model_list: a list of two sklearn classifiers
+    
+    Returns:
+    --------
+    gscv:   A fitted GridSearchCV object.  Score for each cv 
+        is the ROC AUC (only works for binary classes)        
+    
+    """
+    
+    from sklearn.ensemble import VotingClassifier
+    from sklearn.model_selection import GridSearchCV
+
+    # Probably need to import here all classifier types that 
+    # might be sent to this function
+    from sklearn.ensemble import RandomForestClassifier
+    from sklearn.linear_model import LogisticRegression
+    
+    vc = VotingClassifier([('m0', model_list[0]),
+                           ('m1', model_list[1]),
+                           ('m2', model_list[2])],
+                           voting='soft')
+    
+    # put together the parameter list for different weights
+    # for the VotingClassifier
+    grid_size = 4
+    w_array = []
+    for i in np.linspace(0,1,grid_size):
+        for j in np.linspace(0,1,grid_size):
+            for k in np.linspace(0,1,grid_size):
+                if ((i+j+k) < 1 + 1e-14) and ((i+j+k) > 1 - 1e-14 ):
+                    w_array.append([i,j,k])
+    
+    gs_params = {'weights': w_array}
+
     gscv = GridSearchCV(vc, gs_params,
                         cv=5, return_train_score=True, scoring = 'roc_auc')
     gscv.fit(X,y)
@@ -75,19 +126,19 @@ def ensemble_two_models(X, y, model_list):
 
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-def CV_test_rig(X,y):
+def CV_test_rig_2rfc(X,y):
     """
-    Tests GridSearchV with VotingClassifier composed of two 
+    Tests GridSearchCV with a VotingClassifier composed of two 
     RandomForestClassifiers
     
     Parameters:
     ----------
     X: predictors, sklearn format
-    y: targets, sklarn format
+    y: targets, sklearn format
     
     Returns:
     --------
-    gscv: a fitted GridSearchCv object
+    gscv: a fitted GridSearchCV object
     """
     
     from sklearn.ensemble import RandomForestClassifier
@@ -104,19 +155,36 @@ def CV_test_rig(X,y):
 
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-"""
-Test rig for GridSearch on ensemble classifier, built w/ iris data
-"""
+def CV_test_rig_3rfc(X,y):
+    """
+    Tests GridSearchCV with a VotingClassifier composed of three
+    RandomForestClassifiers
+    
+    Parameters:
+    ----------
+    X: predictors, sklearn format
+    y: targets, sklearn format
+    
+    Returns:
+    --------
+    gscv: a fitted GridSearchCV object
+    """
+    
+    from sklearn.ensemble import RandomForestClassifier
+    from sklearn.model_selection import GridSearchCV
+    
+    rfc1 = RandomForestClassifier(n_estimators = 5, max_depth = 4)
+    rfc2 = RandomForestClassifier(n_estimators = 4, max_depth = 5)
+    rfc3 = RandomForestClassifier(n_estimators = 10, max_depth = 2)
+    
+    from sklearn.model_selection import GridSearchCV
+    
+    gscv = ensemble_three_models(X,y, [rfc1, rfc2, rfc3])
 
-from sklearn.datasets import load_iris
-
-data = load_iris()
-iris_y = data.target
-iris_X = data.data
-
-gscv = CV_test_rig(X,y)
+    return gscv
 
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
 
 """
 Test rig for GridSearch on ensemble classifier, built w/ breast-cancer
@@ -128,7 +196,8 @@ data = load_breast_cancer()
 y = data.target
 X = data.data
 
-gscv = CV_test_rig(X,y)
+gscv = CV_test_rig_3rfc(X,y)
 
-gscv.cv_results_
+cv_results_df = pd.DataFrame(gscv.cv_results_)
 gscv.best_score_
+gscv.best_params_
